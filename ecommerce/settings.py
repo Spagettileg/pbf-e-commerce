a15b9da201d4646f2ca1095b4518eeb677a8f16f
Ubuntu <ubuntu@ip-172-31-92-7.ec2.internal>
2019-11-07 09:44:39 +0000

    Products added to installed apps to help enable 'makemigrations' and 'migrate' commands in bash ubuntu

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -38,8 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.forms.bootstrap',
+    'django_forms_bootstrap',
     'accounts',
+    'products',
 ]
 
 MIDDLEWARE = [
