5a993cf3b512b87926a8f45f259ddfb26e6286e5
Ubuntu <ubuntu@ip-172-31-92-7.ec2.internal>
2019-11-08 14:01:56 +0000

    If statement added to avoid overwriting a cart value when the same product is added to the cart

--- cart/views.py
+++ cart/views.py
@@ -14,7 +14,10 @@ def add_to_cart(request, id):
     quantity=int(request.POST.get('quantity'))
     
     cart = request.session.get('cart', {})
-    cart[id] = cart.get(id, quantity)
+    if id in cart:
+        cart[id] = int(cart[id]) + quantity      
+    else:
+        cart[id] = cart.get(id, quantity) 
     
     request.session['cart'] = cart
     return redirect(reverse('index'))
