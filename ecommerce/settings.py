9ddf7f72e011805901cac60d6d589bccf4c51c3c
Ubuntu <ubuntu@ip-172-31-92-7.ec2.internal>
2019-11-08 13:36:04 +0000

    A new context 'card_contents' added to templates

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -68,6 +68,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.media',
+                'cart.contexts.cart_contents',
             ],
         },
     },
