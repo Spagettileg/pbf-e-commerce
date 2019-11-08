f6416804586ca8d01de6b01cb04941eb81009070
Ubuntu <ubuntu@ip-172-31-92-7.ec2.internal>
2019-11-08 13:35:06 +0000

    Views added for view_cart, add_to_cart & adjust_cart

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 
-# Create your views here.
+def view_cart(request):
+    """
+    A view that renders the cart showing all contents page
+    """
+    return render(request, "cart.html")
+    
+def add_to_cart(request, id):
+    """
+    Add a quantity of the specified product to the cart within the session.
+    Cart item are not stored in the database, but in the cart.
+    """
+    quantity=int(request.POST.get('quantity'))
+    
+    cart = request.session.get('cart', {})
+    cart[id] = cart.get(id, quantity)
+    
+    request.session['cart'] = cart
+    return redirect(reverse('index'))
+    
+def adjust_cart(request, id):
+    """
+    Adjust the quantity of the specified product to the specified amount
+    """
+    quantity=int(request.POST.get('quantity'))
+    cart = request.session.get('cart', {})
+    
+    if quantity > 0:  # Cart adjustments permitted with cart size > 0 
+        cart[id] = quantity
+    else:
+        cart.pop(id)
+        
+    request.session['cart'] = cart
+    return redirect(reverse('view_cart'))
+    
