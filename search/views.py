24a9c054d51489ee8074e098dd807123707b85b5
Ubuntu <ubuntu@ip-172-31-92-7.ec2.internal>
2019-11-11 21:30:12 +0000

    New app called search added to help user search for products

---
+++ search/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from products.models import Product
+
+def do_search(request):
+    products = Product.object.filter(name__icontains=request.GET['q']) 
+    return render(request, "products.html", {"products": products})
+    
+
