d42bce4fd03dc5ff91e9681bc3e1e9b847eece62
Ubuntu <ubuntu@ip-172-31-92-7.ec2.internal>
2019-11-19 21:41:54 +0000

    Registration URL changed to Register URL

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import url, include
-from accounts.views import index, logout, login, registration, user_profile
+from accounts.views import index, logout, login, register, user_profile
 from accounts import urls_reset
 
 urlpatterns = [
     url(r'^logout/', logout, name="logout"),
     url(r'^login/', login, name="login"),
-    url(r'^register/', registration, name="registration"),
+    url(r'^register/', register, name="register"),
     url(r'^profile/', user_profile, name="profile"),
     url(r'^password-reset/', include(urls_reset))
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -43,7 +43,7 @@ def login(request):
         login_form = UserLoginForm()
     return render(request, 'login.html', {"login_form": login_form})
     
-def registration(request):
+def register(request):
     """
     Render the registration page
     """
