4275b7fbbfaf2bbd61d2711173834ee465d7747b
Ubuntu <ubuntu@ip-172-31-36-38.eu-west-1.compute.internal>
2021-03-22 13:17:06 +0000

    Auth views now imported from django.contrib.auth library, in accordancre with latest django library conventions.

--- accounts/urls_reset.py
+++ accounts/urls_reset.py
@@ -1,12 +1,21 @@
 from django.conf.urls import url
 from django.urls import reverse_lazy
-from django.contrib.auth.views import password_reset, password_reset_done, password_reset_confirm, password_reset_complete
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    url('^$', password_reset, {'post_reset_redirect': reverse_lazy('password_reset_done')}, name='password_reset'),
-    url(r'^done/$', password_reset_done, name='password_reset_done'),
-    url(r'^(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$', password_reset_confirm,
-        {'post_reset_redirect': reverse_lazy('password_reset_complete')}, name='password_reset_confirm'),
-    url(r'^complete/$', password_reset_complete, name='password_reset_complete')
-]
-    
+    url(r'^password-reset/$', auth_views.PasswordResetView.as_view(
+        template_name='password_reset_form.html'),
+        name='password_reset'),
+    url(r'^password-reset-done/$', auth_views.PasswordResetDoneView.as_view(
+        template_name='password_reset_done.html'),
+        name='password_reset_done'),
+    url(r'^(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$',
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name='password_reset_confirm.html'),
+        {'post_reset_redirect': reverse_lazy('password_reset_complete')},
+        name='password_reset_confirm'),
+    url(r'^password-reset-complete/$',
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name='password_reset_complete.html'),
+        name='password_reset_complete'),
+] 
